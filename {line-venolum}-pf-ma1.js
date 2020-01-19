9393acf0f94e68b43cefcabccc76d0eaed45fa7b
LineVenolum <57433338+LineVenolum@users.noreply.github.com>
2020-01-19 21:41:43 +0100

    Add files via upload

---
+++ {line-venolum}-pf-ma1.js
@@ -0,0 +1,31 @@
+/*question 1*/
+var firstVar 
+
+/*question 2*/
+var name = "Line";
+
+/*question 3*/
+var age = 23; 
+
+/*question 4*/
+var division = 20 / 5;
+
+/*question 5*/
+var animal ="frog";
+
+typeof(animal);
+
+/*question 6*/
+var orderHasShipped = true;
+
+/*question 7*/
+if (orderHasShipped === true) {
+    console.log("Order has been shipped");
+} else {
+    console.log("Order has not been shipped");
+}
+
+/*question 8*/
+for (var count = 0; count <10; count++) {
+    console.log (count);
+}
